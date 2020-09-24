6630b662a695c9f612c962f6fcae33a7e4becce8
jtaylorcpp <jtaylorcpp@gmail.com>
2020-09-23 20:30:05 -0700

    adding client code

--- agent/client.go
+++ agent/client.go
@@ -1,10 +1,54 @@
 package agent
 
-import "github.com/jtaylorcpp/secql/graph/model"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
 
-type Client struct{}
+	"github.com/jtaylorcpp/secql/graph/model"
+)
 
-func GetOSInfo() (model.OSInfo, error) {
+type Client struct {
+	host       string
+	httpClient *http.Client
+}
+
+func NewClient(host string) *Client {
+	tr := &http.Transport{
+		MaxIdleConns:       10,
+		IdleConnTimeout:    30 * time.Second,
+		DisableCompression: true,
+	}
+	return &Client{
+		host: host,
+		httpClient: &http.Client{
+			Transport: tr,
+		},
+	}
+}
+
+func (c *Client) GetOSInfo() (model.OSInfo, error) {
+	resp, err := c.httpClient.Get(c.host + "/os_info")
+	if err != nil {
+		return model.OSInfo{}, err
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return model.OSInfo{}, err
+	}
+
+	var osInfos []model.OSInfo
+	err = json.Unmarshal(bodyBytes, &osInfos)
+	if err != nil {
+		return model.OSInfo{}, err
+	}
+
+	if len(osInfos) > 0 {
+		return osInfos[0], nil
+	}
+
 	return model.OSInfo{}, nil
 }
 
@@ -12,6 +56,6 @@ func GetOSPackages() ([]model.OSPackage, error) {
 	return []model.OSPackage{}, nil
 }
 
-func GetListeningApplications([]model.ListeningApplications, error) {
-	return []model.ListeningApplications{}, nil
+func GetListeningApplications() ([]model.ListeningApplication, error) {
+	return []model.ListeningApplication{}, nil
 }
